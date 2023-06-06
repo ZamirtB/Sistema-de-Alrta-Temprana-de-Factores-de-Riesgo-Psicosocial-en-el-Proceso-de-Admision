ce27d7b23997830e5d369f87f1c3d5f1acff5a8c
Zamirt Barrera <hzbarrerar@unac.edu.co>
2023-06-06 18:13:50 -0500

    Modificaciones para el boton

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import { Login} from "./Componentes/Login";
 import { Ficha } from "./Componentes/Ficha";
 import { Login} from "./Componentes/Login";
 
 
 ReactDOM.render(
   <React.StrictMode>
-<<<<<<< HEAD
     
-    <Ficha />
-    
-=======
+   
     <Login />
->>>>>>> 83bad7c8b1dd2e3e173c885345983baab9222e21
+
+    <Ficha />
   </React.StrictMode>,
   document.getElementById('root')
 );
